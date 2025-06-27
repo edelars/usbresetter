8c76a81f7304bc1cb19ede77dd0f0f982fc9f9ff
Arseni Edel <edel.ars@gmail.com>
2025-06-27 09:54:13 -0300

    first commit

---
+++ go.mod
@@ -0,0 +1,13 @@
+module usbresetter
+
+go 1.24.2
+
+require (
+	github.com/go-ping/ping v1.2.0 // indirect
+	github.com/google/gousb v1.1.3 // indirect
+	github.com/google/uuid v1.6.0 // indirect
+	github.com/prometheus-community/pro-bing v0.7.0 // indirect
+	golang.org/x/net v0.41.0 // indirect
+	golang.org/x/sync v0.15.0 // indirect
+	golang.org/x/sys v0.33.0 // indirect
+)
---
+++ main.go
@@ -0,0 +1,81 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"os/signal"
+	"time"
+
+	"github.com/google/gousb"
+	probing "github.com/prometheus-community/pro-bing"
+)
+
+const t1 = time.Second * time.Duration(10)
+
+func main() {
+	ticker := time.NewTicker(t1)
+	pinger, err := probing.NewPinger("192.168.1.1")
+	if err != nil {
+		panic(err)
+	}
+
+	// Listen for Ctrl-C.
+	c := make(chan os.Signal, 1)
+	signal.Notify(c, os.Interrupt)
+	go func() {
+		for range c {
+			pinger.Stop()
+			ticker.Stop()
+		}
+	}()
+
+	pinger.OnRecv = func(pkt *probing.Packet) {
+		fmt.Printf("%d bytes from %s: icmp_seq=%d time=%v\n",
+			pkt.Nbytes, pkt.IPAddr, pkt.Seq, pkt.Rtt)
+	}
+	pinger.Count = 3
+
+	pinger.OnFinish = func(stats *probing.Statistics) {
+		if stats.PacketLoss == 3 {
+			reset()
+		}
+	}
+
+	fmt.Println("usbresetter started")
+
+	for {
+		select {
+		case <-ticker.C:
+			pinger.Count = 3
+
+			err = pinger.Run()
+			if err != nil {
+				fmt.Println(err)
+			}
+
+			ticker.Reset(t1)
+		}
+	}
+}
+
+func reset() {
+	ctx := gousb.NewContext()
+	defer ctx.Close() // Ensure the context is closed when done
+
+	devs, err := ctx.OpenDevices(func(desc *gousb.DeviceDesc) bool {
+		return desc.Vendor == 0x0bda && desc.Product == 0xf179
+	})
+	if err != nil {
+		fmt.Println("no device found")
+		// Handle error
+	}
+	if len(devs) == 0 {
+		fmt.Println("no device found: len 0")
+		// Handle case where device is not found
+	}
+
+	// Assuming you want to reset the first found device
+	dev := devs[0]
+	fmt.Println("Trying to reset %w", dev.Reset().Error())
+	defer dev.Close() // Close the device handle when done
+}
